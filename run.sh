a933f46d826bb442b3ab5027d6af8a64d9f21123
ztjiaweixu <ztjiaweixu@tencent.com>
2022-03-31 19:49:02 +0800

    add finite context

--- run.sh
+++ run.sh
@@ -8,12 +8,13 @@ update_num=100
 repeat_num=500
 time_period=200
 n_expe=100
+n_context=0
 optim=Adam
 
 tag=$(date "+%Y%m%d%H%M%S")
 python main.py --game ${game} \
     --lr=${lr} --noise-dim=${noise_dim} --batch-size=${batch_size} \
-    --update-num=${update_num} --repeat-num=${repeat_num} \
+    --update-num=${update_num} --repeat-num=${repeat_num} --n-context=${n_context} \
     --time-period=${time_period} --n-expe=${n_expe} --optim=${optim} \
     > ~/logs/${game}_${tag}_3.out 2> ~/logs/${game}_${tag}_3.err &
 echo "run $game $tag"
